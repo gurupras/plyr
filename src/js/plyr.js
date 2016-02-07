b0b74898263794a13e2f45d3490568fca149bab3
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2016-02-07 22:49:45 +0000

    Added container to setup return

--- src/js/plyr.js
+++ src/js/plyr.js
@@ -2651,6 +2651,7 @@
         }
 
         return {
+            container:          plyr.container,
             media:              _media,
             play:               _play,
             pause:              _pause,
